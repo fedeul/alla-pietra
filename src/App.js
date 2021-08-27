d4cad1d5f3883146bac18714c45b261711c63e71
Fedeul <fede.ulbrich@gmail.com>
2021-08-27 15:07:36 -0600

    tailwind instalation

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>I'm Fede</h1>
+        <p>
+          <code>Almacen Panadero</code> is coming soon.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with Coder House
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
