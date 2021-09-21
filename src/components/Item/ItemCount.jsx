8edee610f60cac34cb39a58639f8eb8ff1b5f420
Fedeul <fede.ulbrich@gmail.com>
2021-09-21 17:19:50 -0600

    Desafío eventos - Botón del ItemCount

--- src/components/Item/ItemCount.jsx
+++ src/components/Item/ItemCount.jsx
@@ -1,6 +1,65 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const ButtonGoCart = () => {
+  return (
+    <Link to="/cart">
+      <button
+        onClick={() => console.log("cart")}
+        className="bg-green-500 shadow-md  flex place-items-center opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-8 py-2 font-semibold"
+      >
+        GO TO
+        <svg
+          className="w-5 h-7 ml-4"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </Link>
+  );
+};
+
+const ButtonAddToCart = ({ handleInter }) => {
+  return (
+    <button
+      onClick={handleInter}
+      className="bg-yellow-400 shadow-md  flex place-items-center opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-7 py-2 font-semibold"
+    >
+      ADD TO
+      <svg
+        className="w-5 h-5 ml-4"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+  );
+};
 
 const ItemCount = () => {
+  // ===== PARA USAR EN FUTUROS CAMPOS DE INPUT ===== //
+  // const [ButtonType, setButtonType] = useState("button");
+  // const handleInter = () => {
+  //   setButtonType("button");
+  // };
+
   let price = 20;
 
   const [stock, setStock] = useState({
@@ -47,24 +106,13 @@ const ItemCount = () => {
             className=" bg-gray-200 text-blue-600 pb-1  rounded-full mx-2 w-11"
           >
             -
-          </button>
-          <button className="bg-yellow-400 shadow-md  flex place-items-center opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold">
-            <svg
-              className="w-5 h-5 mr-4"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-            ADD
-          </button>
+          </button>{" "}
+          {stock.item > 0 ? (
+            <ButtonGoCart />
+          ) : (
+            <ButtonAddToCart />
+            // <ButtonAddToCart handleInter={handleInter} />
+          )}
         </div>
       </div>
       <h1 className="text-gray-700 text-center align-baseline">
