29a9a8c18c9bc2185539a6aaba2f66a86a151faf
Federico Ulbrich <63756946+fedeul@users.noreply.github.com>
2022-04-10 16:00:25 -0700

    Update getFirebase.js

--- src/service/getFirebase.js
+++ src/service/getFirebase.js
@@ -1,5 +1,7 @@
-import firebase from "firebase";
+// import firebase from "firebase";
 import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/<PACKAGE>';
 
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
