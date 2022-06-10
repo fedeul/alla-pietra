4a669adabe11f054d22162ebcf48ab3cd3e39a4a
Dashboard Design <fede.ulbrich@gmail.com>
2022-06-10 13:57:39 -0600

    connection fixed

--- src/service/getFirebase.js
+++ src/service/getFirebase.js
@@ -4,33 +4,17 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: process.env.APIKEY,
-//   authDomain: process.env.authDomain,
-//   projectId: process.env.projectId,
-//   storageBucket: process.env.storageBucket,
-//   messagingSenderId: process.env.messagingSenderId,
-//   appId: process.env.appId,
-// };
-
-// const app = firebase.initializeApp(firebaseConfig);
-
-// export function getFirestore() {
-//   return firebase.firestore(app);
-// }
-
-import { initializeApp } from "firebase/app";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD4CD8Uyz5LsGcEsJrRtkiIg7rv_2FAzJI",
-  authDomain: "almacenpanadero.firebaseapp.com",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.authDomain,
+  // projectId: process.env.projectId,
   projectId: "almacenpanadero",
-  storageBucket: "almacenpanadero.appspot.com",
-  messagingSenderId: "439195503589",
-  appId: "1:439195503589:web:34c23c6899503d84ad7d0c",
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 export function getFirestore() {
   return firebase.firestore(app);
